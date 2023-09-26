c6a1a77b049aeb299d5dca1bd48dcebd5a9e6576
ajulianavil <54120274+ajulianavil@users.noreply.github.com>
2023-09-26 14:58:29 -0500

    update python-dotenv

--- s3_file_selector/settings.py
+++ s3_file_selector/settings.py
@@ -79,13 +79,6 @@ WSGI_APPLICATION = 's3_file_selector.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 load_dotenv()
 
 # Password validation
